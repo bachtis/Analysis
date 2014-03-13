4f31413a62c22a28afb6ea7a729cd527284fd8ea
Michail <bachtis@cern.ch>
2014-03-13 23:49:16 +0100

    Now adding the module

--- CMGTools/MuonCalibration/src/LinkDef.h
+++ CMGTools/MuonCalibration/src/LinkDef.h
@@ -1,5 +1,6 @@
 #include "../interface/Roo3DMapVar.h"
 #include "../interface/RooGaussianSumPdf.h"
+#include "../interface/RooGaussianSumPdf2D.h"
 #include "../interface/RooPosNegBiasEstimator.h"
 
 #ifdef __CINT__
@@ -7,6 +8,7 @@
 #pragma link off all classes;
 #pragma link off all functions;
 #pragma link C++ class RooGaussianSumPdf+;
+#pragma link C++ class RooGaussianSumPdf2D+;
 #pragma link C++ class Roo3DMapVar+;
 #pragma link C++ class RooPosNegBiasEstimator+;
 #endif
