052c38594eab4a1f5692a88b810dbb41fab6690b
Michail <bachtis@cern.ch>
2015-07-19 23:32:48 +0200

    Update for W like preapproval

--- CMGTools/KalmanCalibration/src/classes.h
+++ CMGTools/KalmanCalibration/src/classes.h
@@ -1,8 +1,10 @@
 #include "DataFormats/Common/interface/Wrapper.h"
 #include "CMGTools/KalmanCalibration/interface/KalmanCalibrator.h"
+#include "CMGTools/KalmanCalibration/interface/KalmanCalibratorParam.h"
 
 namespace {
   struct CMGTools_KalmanCalibration {
     class KalmanCalibrator;
+    class KalmanCalibratorParam;
   };
 }
